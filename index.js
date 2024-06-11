d7b56083f05265a81c62ea494bbd2b1301f499f5
Ziad Teama <ziadbasaria600@gmail.com>
2024-06-11 04:15:28 +0300

    Delete feature added

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 const port = 3000;
 var name;
 var avatar;
-var blogs = [];
+var blogs = [{ id: 0, text: 1 },{ id: 1, text: 2 },{ id: 2, text: 3 },{ id: 3, text: 4 },{ id: 4, text: 5 },];
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
@@ -15,26 +15,27 @@ app.get("/", (req, res) => {
 });
 
 app.post("/home", (req, res) => {
-   name = req.body["userName"];
-   avatar = generator.generateRandomAvatar();
+  name = req.body["userName"];
+  avatar = generator.generateRandomAvatar();
   let data = {
     randomAvatar: avatar,
-    userName: name
+    userName: name,
   };
   res.render("home.ejs", data);
 });
 app.get("/home", (req, res) => {
-   let data = {
-     randomAvatar: avatar,
-     userName: name
-   };
-   res.render("home.ejs", data);
- });
+  let data = {
+    randomAvatar: avatar,
+    userName: name,
+  };
+  res.render("home.ejs", data);
+});
 app.post("/save", (req, res) => {
-  let blogWritten = req.body.postArea;
+  const id = blogs.length + 1;
+  let blogWritten = { id: id, text: req.body.postArea };
   if (blogWritten) blogs.push(blogWritten);
   const dataForPosts = {
-    userBlog: blogs
+    userBlog: blogs,
   };
 });
 
@@ -42,11 +43,15 @@ app.get("/posts", (req, res) => {
   let data = {
     randomAvatar: avatar,
     userName: name,
-    userBlog: blogs
+    userBlog: blogs,
   };
   res.render("posts.ejs", data);
 });
-
+app.post("/deletepost", (req, res) => {
+  const id = parseInt(req.body.id, 10);
+  blogs = blogs.filter(post => post.id !== id);
+  res.redirect("/posts")
+});
 
 app.listen(port, () => {
   console.log(`Running on port ${port}`);
